1ee3b9a05b27a8e52c29ee9d682f2764181baf30
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-07-12 15:35:03 +0900

    Add VPC2 Public VM Security Group

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ data "terraform_remote_state" "network" {
   }
 }
 
-resource "aws_security_group" "public_vm_sg" {
+resource "aws_security_group" "vpc1_public_vm_sg" {
   name        = "public_vm_sg"
   description = "INSIDE_AWS_Public_VM_Security_GROUP"
   vpc_id      = data.terraform_remote_state.network.outputs.vpc01_id
@@ -51,7 +51,7 @@ resource "aws_security_group" "public_vm_sg" {
 resource "aws_security_group_rule" "public_sg_rule_01" {
   from_port         = 22
   protocol          = "tcp"
-  security_group_id = aws_security_group.public_vm_sg.id
+  security_group_id = aws_security_group.vpc1_public_vm_sg.id
   to_port           = 22
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "public_sg_rule_01" {
 resource "aws_security_group_rule" "public_sg_rule_02" {
   from_port         = "-1"
   protocol          = "icmp"
-  security_group_id = aws_security_group.public_vm_sg.id
+  security_group_id = aws_security_group.vpc1_public_vm_sg.id
   to_port           = "-1"
   type              = "ingress"
   cidr_blocks       = ["10.10.10.0/24"]
@@ -69,8 +69,44 @@ resource "aws_security_group_rule" "public_sg_rule_02" {
 resource "aws_security_group_rule" "public_sg_rule_03" {
   from_port         = "-1"
   protocol          = "icmp"
-  security_group_id = aws_security_group.public_vm_sg.id
+  security_group_id = aws_security_group.vpc1_public_vm_sg.id
   to_port           = "-1"
   type              = "egress"
   cidr_blocks       = ["10.10.10.0/24"]
+}
+
+resource "aws_security_group" "vpc2_public_vm_sg" {
+  name        = "public_vm_sg_vpc2"
+  description = "INSIDE_AWS_Public_VM_Security_GROUP"
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc02_id
+
+  tags = local.common-tags
+}
+
+### SSH Port open ANY
+resource "aws_security_group_rule" "vpc2_public_sg_rule_01" {
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc2_public_vm_sg.id
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "vpc2_public_sg_rule_02" {
+  from_port         = "-1"
+  protocol          = "icmp"
+  security_group_id = aws_security_group.vpc2_public_vm_sg.id
+  to_port           = "-1"
+  type              = "ingress"
+  cidr_blocks       = ["10.10.20.0/24"]
+}
+
+resource "aws_security_group_rule" "vpc2_public_sg_rule_03" {
+  from_port         = "-1"
+  protocol          = "icmp"
+  security_group_id = aws_security_group.vpc2_public_vm_sg.id
+  to_port           = "-1"
+  type              = "egress"
+  cidr_blocks       = ["10.10.20.0/24"]
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "public-vm-sg-id" {
-  value = aws_security_group.public_vm_sg.id
+output "vpc1-public-vm-sg-id" {
+  value = aws_security_group.vpc1_public_vm_sg.id
+}
+
+output "vpc2-public-vm-sg-id" {
+  value = aws_security_group.vpc2_public_vm_sg.id
 }
