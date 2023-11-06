027cd3a408fe31b1929ecd89ae7154768effc71d
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-11-06 14:53:08 +0900

    modify vpc2 security group

--- main.tf
+++ main.tf
@@ -124,20 +124,20 @@ resource "aws_security_group_rule" "public_sg_rule_07" {
 }
 ########################################## (END) VPC1 PUBLIC VM SG ###########################################
 
-########################################## (START) VPC2 PUBLIC VM SG ###########################################
+########################################## (START) VPC2 PRIVATE VM SG ###########################################
 resource "aws_security_group" "vpc2_public_vm_sg" {
-  name        = "public_vm_sg_vpc2"
-  description = "INSIDE_AWS_Public_VM_Security_GROUP"
+  name        = "private_vm_sg_vpc2"
+  description = "INSIDE_AWS_Private_VM_Security_GROUP"
   vpc_id      = data.terraform_remote_state.network.outputs.vpc02_id
 
   tags = (merge(local.common-tags, tomap({
-    Name     = "vpc2_public_vm_sg"
+    Name     = "vpc2_private_vm_sg"
     resource = "aws_security_group"
   })))
 }
 
 ### SSH Port open ANY
-resource "aws_security_group_rule" "vpc2_public_sg_rule_01" {
+resource "aws_security_group_rule" "vpc2_private_sg_rule_01" {
   from_port         = 22
   protocol          = "tcp"
   security_group_id = aws_security_group.vpc2_public_vm_sg.id
@@ -146,21 +146,30 @@ resource "aws_security_group_rule" "vpc2_public_sg_rule_01" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "vpc2_public_sg_rule_02" {
+resource "aws_security_group_rule" "vpc2_private_sg_rule_02" {
   from_port         = "-1"
   protocol          = "icmp"
   security_group_id = aws_security_group.vpc2_public_vm_sg.id
   to_port           = "-1"
   type              = "ingress"
-  cidr_blocks       = ["10.10.20.0/24"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "vpc2_public_sg_rule_03" {
+resource "aws_security_group_rule" "vpc2_private_sg_rule_03" {
   from_port         = "-1"
   protocol          = "icmp"
   security_group_id = aws_security_group.vpc2_public_vm_sg.id
   to_port           = "-1"
   type              = "egress"
-  cidr_blocks       = ["10.10.20.0/24"]
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "vpc2_private_sg_rule_04" {
+  from_port         = "443"
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc2_public_vm_sg.id
+  to_port           = "443"
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 ########################################## (END) VPC2 PUBLIC VM SG ###########################################
